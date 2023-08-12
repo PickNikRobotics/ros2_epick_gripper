047bf05f264a1df9685f0e073a7307f843141d7a
Giovanni Remigi <g.remigi@kineticsystem.org>
2023-08-12 23:04:25 +0200

    Implemented grip and release actions.

--- epick_driver/src/default_driver.cpp
+++ epick_driver/src/default_driver.cpp
@@ -119,24 +119,24 @@ void DefaultDriver::activate()
   auto timeout_in_hundredths = static_cast<uint8_t>(
       std::chrono::duration_cast<std::chrono::duration<int, std::ratio<1, 100>>>(clamped_gripper_timeout).count());
 
-  uint8_t action_register = 0b00000000;
-  driver_utils::set_gripper_activation_action(action_register, GripperActivationAction::Activate);
-  driver_utils::set_gripper_mode(action_register, gripper_mode_);
+  uint8_t action_request_register = 0b00000000;
+  driver_utils::set_gripper_activation_action(action_request_register, GripperActivationAction::Activate);
+  driver_utils::set_gripper_mode(action_request_register, gripper_mode_);
 
   std::vector<uint8_t> request = {
     slave_address_,
     static_cast<uint8_t>(driver_utils::FunctionCode::PresetMultipleRegisters),
     data_utils::get_msb(kActionRequestRegisterAddress),
     data_utils::get_lsb(kActionRequestRegisterAddress),
-    0x00,                   // Number of registers to write MSB.
-    0x03,                   // Number of registers to write LSB.
-    0x06,                   // Number of bytes to write.
-    action_register,        // Action register.
-    0x00,                   // Reserved.
-    0x00,                   // Reserved.
-    max_absolute_pressure,  // Max absolute pressure.
-    timeout_in_hundredths,  // Gripper Timeout.
-    min_absolute_pressure   // Min absolute pressure
+    0x00,                     // Number of registers to write MSB.
+    0x03,                     // Number of registers to write LSB.
+    0x06,                     // Number of bytes to write.
+    action_request_register,  // Action register.
+    0x00,                     // Reserved.
+    0x00,                     // Reserved.
+    max_absolute_pressure,    // Max absolute pressure.
+    timeout_in_hundredths,    // Gripper Timeout.
+    min_absolute_pressure     // Min absolute pressure
   };
   auto crc = crc_utils::compute_crc(request);
   request.push_back(data_utils::get_msb(crc));
@@ -189,10 +189,65 @@ void DefaultDriver::deactivate()
 
 void DefaultDriver::grip()
 {
+  GripperStatus gripper_status = get_status();
+  uint8_t action_request_register = 0b00000000;
+  driver_utils::set_gripper_activation_action(action_request_register, gripper_status.gripper_activation_action);
+  driver_utils::set_gripper_mode(action_request_register, gripper_status.gripper_mode);
+  driver_utils::set_gripper_regulate_action(action_request_register,
+                                            GripperRegulateAction::FollowRequestedVacuumParameters);
+
+  std::vector<uint8_t> request = { slave_address_,
+                                   static_cast<uint8_t>(driver_utils::FunctionCode::PresetSingleRegister),
+                                   data_utils::get_msb(kActionRequestRegisterAddress),
+                                   data_utils::get_lsb(kActionRequestRegisterAddress),
+                                   action_request_register,
+                                   0x00 };
+  auto crc = crc_utils::compute_crc(request);
+  request.push_back(data_utils::get_msb(crc));
+  request.push_back(data_utils::get_lsb(crc));
+
+  std::vector<uint8_t> response;
+  try
+  {
+    serial_interface_->write(request);
+    response = serial_interface_->read(kWriteResponseSize);
+  }
+  catch (const serial::IOException& e)
+  {
+    RCLCPP_ERROR(kLogger, "Failed to grip: %s", e.what());
+    throw;
+  }
 }
 
 void DefaultDriver::release()
 {
+  GripperStatus gripper_status = get_status();
+  uint8_t action_request_register = 0b00000000;
+  driver_utils::set_gripper_activation_action(action_request_register, gripper_status.gripper_activation_action);
+  driver_utils::set_gripper_mode(action_request_register, gripper_status.gripper_mode);
+  driver_utils::set_gripper_regulate_action(action_request_register, GripperRegulateAction::StopVacuumGenerator);
+
+  std::vector<uint8_t> request = { slave_address_,
+                                   static_cast<uint8_t>(driver_utils::FunctionCode::PresetSingleRegister),
+                                   data_utils::get_msb(kActionRequestRegisterAddress),
+                                   data_utils::get_lsb(kActionRequestRegisterAddress),
+                                   action_request_register,
+                                   0x00 };
+  auto crc = crc_utils::compute_crc(request);
+  request.push_back(data_utils::get_msb(crc));
+  request.push_back(data_utils::get_lsb(crc));
+
+  std::vector<uint8_t> response;
+  try
+  {
+    serial_interface_->write(request);
+    response = serial_interface_->read(kWriteResponseSize);
+  }
+  catch (const serial::IOException& e)
+  {
+    RCLCPP_ERROR(kLogger, "Failed to release: %s", e.what());
+    throw;
+  }
 }
 
 void DefaultDriver::set_mode(const GripperMode gripper_mode)
