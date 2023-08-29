e742f63ddd7bb82e9d86c8eaff266dd3edf62bdc
Giovanni Remigi <g.remigi@kineticsystem.org>
2023-08-29 11:50:10 +0200

    Made timeout const.

--- epick_driver/src/default_driver.cpp
+++ epick_driver/src/default_driver.cpp
@@ -156,7 +156,7 @@ void DefaultDriver::activate()
 
   const uint8_t grip_max_absolute_pressure = static_cast<uint8_t>(kAtmosphericPressure);
   const uint8_t grip_min_absolute_pressure = static_cast<uint8_t>(kAtmosphericPressure);
-  auto tiemout = static_cast<uint8_t>(
+  const auto tiemout = static_cast<uint8_t>(
       std::chrono::duration_cast<std::chrono::duration<int, std::ratio<1, 100>>>(
           std::clamp(grip_timeout_, std::chrono::milliseconds(0), std::chrono::milliseconds(kMaxTimeout)))
           .count());
